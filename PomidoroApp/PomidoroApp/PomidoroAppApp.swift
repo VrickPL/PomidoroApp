28c8a81d651c43ee89b0241bbd4642f55b56cff0
Jan Kazubski <jkazubski@icloud.com>
2024-07-18 17:42:20 +0200

    first commit

---
+++ PomidoroApp/PomidoroApp/PomidoroAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomidoroAppApp.swift
+//  PomidoroApp
+//
+//  Created by Jan Kazubski on 18/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct PomidoroAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
